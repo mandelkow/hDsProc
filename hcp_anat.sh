b7cc8aaac591350d958e4819ab4cbbbaef90c37c
HM <Hendrik.Mandelkow@nih.gov>
2021-03-08 19:35:26 -0500

    +++ hcp_anat -c/l/s

--- hcp_anat.sh
+++ hcp_anat.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# hcp_anat.sh [-c|-l] source_dir [dest_dir]
 
+if [[ $1 == -* ]]; then MODE=$1 ; shift ; fi
 SRC=`readlink -e $1`
-DST=`readlink -e $2`
+if [[ -z $2 ]]; then DST=$PWD ; else DST=`readlink -e $2` ; fi
 cd $SRC
-# TEST: for F in sub-*/ses-*/run-* ; do echo cp -avrl $SRC/$F/anat_warp $DST/$F ; done
-for F in sub-*/ses-*/run-* ; do cp -avrl $SRC/$F/anat_warp $DST/$F ; done
+if [[ $MODE == "-c" ]]; then
+    for F in sub-*/ses-*/run-* ; do cp -avr $SRC/$F/anat_warp $DST/$F ; done
+elif [[ $MODE == "-l" ]]; then
+    for F in sub-*/ses-*/run-* ; do cp -avrl $SRC/$F/anat_warp $DST/$F ; done
+else
+    for F in sub-*/ses-*/run-*/anat_warp ; do ln -vs $SRC/$F $DST/$F ; done
+fi
 cd -
